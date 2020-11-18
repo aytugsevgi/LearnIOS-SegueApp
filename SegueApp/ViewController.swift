feec57e0b58b61278fd72b0c136270aa80a067a3
aytug.sevgi <aytug3667@gmail.com>
2020-11-19 01:08:24 +0300

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by aytug on 19.11.2020.
+//  Copyright © 2020 aytug. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
